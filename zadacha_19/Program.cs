dc5bec6872cd9c6da737941a80213f52d4c87405
Elena <9513495670@mail.ru>
2022-07-20 16:14:08 +0300

    added zadacha_19

---
+++ zadacha_19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Check(int number)
+{
+    if (number>=10000&&number<=99999)return number;
+    else{
+        Console.Write("Введите пятизначное число: ");
+        int newnum = Convert.ToInt32(Console.ReadLine());
+        return Check(newnum);
+    }
+}
+int cor_num = Check(num);
+int Palindrome (int numb)
+{
+    int pal_number=0;
+    while(numb>0)
+    {
+        int rem=numb%10;
+        pal_number=pal_number*10+rem;
+        numb=numb/10;
+    }
+    return pal_number;
+}
+int pal_num=Palindrome(cor_num);
+
+if(cor_num==pal_num) Console.WriteLine($"{cor_num} -> да");
+else Console.WriteLine($"{cor_num} -> нет");
